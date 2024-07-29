5e7af1e1441d5a09eabd962fbfd80b058156823b
43529 <43529and43529@gmail.com>
2024-07-30 01:06:12 +0800

    feat: 增加必要组件vuetify和rollup-plugin-copy

---
+++ src/components/Calendar.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="backgroud">
+    <v-card text="calendar"></v-card>
+  </div>
+</template>
+
+<style scoped>
+.backgroud {
+  background-color: bisque;
+  padding: 16px;
+  height: 100vh;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import copy from 'rollup-plugin-copy'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    copy({
+      targets: [
+        { src: 'src/assets/icons/*', dest: 'dist/assets/icons' } // 确认路径是否正确
+      ],
+      hook: 'writeBundle' // 确保在构建后执行
+    })
+  ],
   base: './', // 设置基础路径为相对路径
   build: {
     outDir: 'dist', // 输出目录
-    assetsDir: 'dist' // 资源目录
+    assetsDir: 'assets' // 资源目录
   },
   resolve: {
     alias: {
